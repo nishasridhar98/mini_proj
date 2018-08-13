c29322520cb6c65f441d46d8ed2bcf2c55b4b4e5
Nisha <nisha.sridhar1998@gmail.com>
2018-08-13 20:01:42 +0530

    Added code

--- skewed_names.py
+++ skewed_names.py
@@ -3,3 +3,17 @@
 Students have entered their names with unnecessary special characters and numbers in-between.
 Try and remove these without distorting the names.But do not remove the spaces between their first and last names.'''
 
+name=[]
+a=input("enter ur name: ")
+
+for l in a:
+	if l.isdigit():
+		continue
+	elif l.isalpha() or l==" ":
+		name.append(l)
+	else:
+		continue
+
+s=''.join(name)
+
+print(s)
