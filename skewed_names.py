08043efa8baf2083c2ed9af56dc8e91aad4982ae
Nisha <nisha.sridhar1998@gmail.com>
2018-08-13 19:40:20 +0530

    Defining problem statement

---
+++ skewed_names.py
@@ -0,0 +1,5 @@
+'''Processing Skewed Names'''
+'''Problem Statement:
+Students have entered their names with unnecessary special characters and numbers in-between.
+Try and remove these without distorting the names.But do not remove the spaces between their first and last names.'''
+
