9b83473cabf5669724f7cb688c14d16775ad537c
Nisha <nisha.sridhar1998@gmail.com>
2018-08-13 20:49:55 +0530

    Added the code

--- blog_titles.py
+++ blog_titles.py
@@ -3,3 +3,17 @@ A title to a blog is written with each letter of the first word as a capital let
 But no spaces are given.Try and figure out the words and separate them by spaces.
 '''
 
+n =[]
+words = input("enter the title: ")
+
+n.append(words[0])
+
+for l in words[1:]:
+	if l.isupper():
+		n.append(" ")
+		n.append(l)
+	else:
+		n.append(l)
+
+s=''.join(n)
+print(s)
