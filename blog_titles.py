b444efabe7dc9c21aaf1a4a384d102b2622a27dc
Nisha <nisha.sridhar1998@gmail.com>
2018-08-13 20:45:24 +0530

    Defining problem statement

---
+++ blog_titles.py
@@ -0,0 +1,5 @@
+'''Fixing Blog Titles
+A title to a blog is written with each letter of the first word as a capital letter. 
+But no spaces are given.Try and figure out the words and separate them by spaces.
+'''
+
